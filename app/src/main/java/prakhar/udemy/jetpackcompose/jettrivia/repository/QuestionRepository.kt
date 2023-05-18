dac5448f374a32e7a62ff1ada688df8a067e86e2
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-05-19 00:35:30 +0530

    Wrapping "listOfQuestions" data with Class Wrapper For Data

--- app/src/main/java/prakhar/udemy/jetpackcompose/jettrivia/repository/QuestionRepository.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jettrivia/repository/QuestionRepository.kt
@@ -1,5 +1,6 @@
 package prakhar.udemy.jetpackcompose.jettrivia.repository
 
+import prakhar.udemy.jetpackcompose.jettrivia.data.DataOrException
 import prakhar.udemy.jetpackcompose.jettrivia.model.QuestionItem
 import prakhar.udemy.jetpackcompose.jettrivia.network.QuestionApi
 import javax.inject.Inject
@@ -7,5 +8,8 @@ import javax.inject.Inject
 class QuestionRepository @Inject constructor(
     private val api: QuestionApi
 ) {
-    private val listOfQuestions = ArrayList<QuestionItem>(emptyList())
+    private val listOfQuestions =
+        DataOrException<ArrayList<QuestionItem>,
+                Boolean,
+                Exception>()
 }
