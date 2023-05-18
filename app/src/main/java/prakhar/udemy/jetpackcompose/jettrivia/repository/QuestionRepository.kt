12aa41adfc78cd2125994d3c9bc94388dee4c310
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-05-19 01:04:46 +0530

    Finished the "QuestionRepository" class

--- app/src/main/java/prakhar/udemy/jetpackcompose/jettrivia/repository/QuestionRepository.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jettrivia/repository/QuestionRepository.kt
@@ -1,5 +1,6 @@
 package prakhar.udemy.jetpackcompose.jettrivia.repository
 
+import android.util.Log
 import prakhar.udemy.jetpackcompose.jettrivia.data.DataOrException
 import prakhar.udemy.jetpackcompose.jettrivia.model.QuestionItem
 import prakhar.udemy.jetpackcompose.jettrivia.network.QuestionApi
@@ -8,8 +9,23 @@ import javax.inject.Inject
 class QuestionRepository @Inject constructor(
     private val api: QuestionApi
 ) {
-    private val listOfQuestions =
+    private val dataOrException =
         DataOrException<ArrayList<QuestionItem>,
                 Boolean,
                 Exception>()
+
+    suspend fun getAllQuestion(): DataOrException<ArrayList<QuestionItem>, Boolean, Exception> {
+
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+        } catch (exception: Exception) {
+            dataOrException.e = exception
+            Log.d("Exc", "getAllQuestion: ${dataOrException.e!!.localizedMessage}")
+        }
+
+        return dataOrException
+    }
+
 }
