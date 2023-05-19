1afa3a5ac2b3bea8a723d698e1f6c5be8a055454
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-05-20 01:51:04 +0530

    Created UI - Dotted Line Composable

--- app/src/main/java/prakhar/udemy/jetpackcompose/jettrivia/component/Questions.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jettrivia/component/Questions.kt
@@ -1,9 +1,12 @@
 package prakhar.udemy.jetpackcompose.jettrivia.component
 
 import android.util.Log
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
@@ -11,6 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -40,6 +45,7 @@ fun Questions(viewModel: QuestionsViewModel) {
 @Preview
 @Composable
 fun QuestionDisplay() {
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f, 0f))
     Surface(
         modifier = Modifier
             .fillMaxSize()
@@ -51,6 +57,7 @@ fun QuestionDisplay() {
             horizontalAlignment = Alignment.Start
         ) {
             QuestionTracker()
+            DrawDottedLine(pathEffect)
         }
     }
 }
@@ -86,4 +93,20 @@ fun QuestionTracker(
         },
         modifier = Modifier.padding(20.dp)
     )
+}
+
+@Composable
+fun DrawDottedLine(pathEffect: PathEffect) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+    ) {
+        drawLine(
+            color = AppColors.mLightGray,
+            start = Offset(x = 0f, y = 0f),
+            end = Offset(x = size.width, y = 0f),
+            pathEffect = pathEffect
+        )
+    }
 }
