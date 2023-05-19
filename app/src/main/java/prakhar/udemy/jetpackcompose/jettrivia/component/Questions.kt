5991fa2fe93b5ff061ba863712be80b4797c1455
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-05-20 02:30:45 +0530

    Creating UI - Radio Button Row

--- app/src/main/java/prakhar/udemy/jetpackcompose/jettrivia/component/Questions.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jettrivia/component/Questions.kt
@@ -2,19 +2,29 @@ package prakhar.udemy.jetpackcompose.jettrivia.component
 
 import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
@@ -22,9 +32,9 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import prakhar.udemy.jetpackcompose.jettrivia.model.QuestionItem
 import prakhar.udemy.jetpackcompose.jettrivia.screens.QuestionsViewModel
 import prakhar.udemy.jetpackcompose.jettrivia.util.AppColors
 
@@ -42,9 +52,17 @@ fun Questions(viewModel: QuestionsViewModel) {
     }
 }
 
-@Preview
+//@Preview
 @Composable
-fun QuestionDisplay() {
+fun QuestionDisplay(
+    question: QuestionItem,
+    questionIndex: MutableState<Int>,
+    viewModel: QuestionsViewModel,
+    onNextClicked: (Int) -> Unit
+) {
+    val choicesState = remember(question) {
+        question.choices.toMutableList()
+    }
     val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f, 0f))
     Surface(
         modifier = Modifier
@@ -58,6 +76,50 @@ fun QuestionDisplay() {
         ) {
             QuestionTracker()
             DrawDottedLine(pathEffect)
+            Column {
+
+                Text(
+                    text = "prakhar",
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .align(alignment = Alignment.Start)
+                        .fillMaxHeight(0.3f),
+                    fontSize = 17.sp,
+                    color = AppColors.mOffWhite,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 22.sp
+                )
+
+                //Choices
+                choicesState.forEachIndexed { index, answerText ->
+                    Row(
+                        modifier = Modifier
+                            .padding(3.dp)
+                            .fillMaxWidth()
+                            .height(45.dp)
+                            .border(
+                                width = 4.dp, brush = Brush.linearGradient(
+                                    colors = listOf(
+                                        AppColors.mOffDarkPurple,
+                                        AppColors.mOffDarkPurple
+                                    )
+                                ),
+                                shape = RoundedCornerShape(15.dp)
+                            )
+                            .clip(
+                                RoundedCornerShape(
+                                    topStartPercent = 50,
+                                    topEndPercent = 50,
+                                    bottomStartPercent = 50,
+                                    bottomEndPercent = 50
+                                )
+                            )
+                            .background(Color.Transparent),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {}
+                }
+
+            }
         }
     }
 }
